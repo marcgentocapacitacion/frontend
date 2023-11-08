eb66abced0d2bfda03e6affbcb2210c84ddfa6e7
Marcos Huerta Hernández <marcgento2@gmail.com>
2023-11-08 00:40:24 -0600

    actualizando archivo

--- tema/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/BicicletasMilan/tema',
-    __DIR__
-);
